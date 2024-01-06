aa2687812923562f3e8a4c3d1b0a2f3435b21d88
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-06 13:05:31 +0200

    Add files via upload

---
+++ Ex2/Program.cs
@@ -0,0 +1,39 @@
+﻿//Se da o secventa de n numere. Sa se determina cate sunt negative, cate sunt zero si cate sunt pozitive. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti lungimea secventei: ");
+        int lungime = Convert.ToInt32(Console.ReadLine());
+
+        int negative = 0, zero = 0, pozitive = 0;
+
+        Console.WriteLine("Introduceti secventa de numere:");
+        for (int i = 0; i < lungime; i++)
+        {
+            int numar = Convert.ToInt32(Console.ReadLine());
+
+            if (numar < 0)
+            {
+                negative++;
+            }
+            else if (numar == 0)
+            {
+                zero++;
+            }
+            else
+            {
+                pozitive++;
+            }
+        }
+
+        Console.WriteLine($"Numere negative: {negative}");
+        Console.WriteLine($"Numere zero: {zero}");
+        Console.WriteLine($"Numere pozitive: {pozitive}");
+
+        Console.ReadKey();
+    }
+}
