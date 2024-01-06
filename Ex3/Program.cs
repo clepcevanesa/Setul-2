0f948f30aae53a8b2dd8d2e5c9032e6fad882efd
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-06 13:06:22 +0200

    Add files via upload

---
+++ Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿//Calculati suma si produsul numerelor de la 1 la n. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduceti n: ");
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int suma = 0;
+        int produs = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            suma += i;
+            produs *= i;
+        }
+
+        Console.WriteLine($"Suma numerelor de la 1 la {n} este: {suma}");
+        Console.WriteLine($"Produsul numerelor de la 1 la {n} este: {produs}");
+    }
+}
