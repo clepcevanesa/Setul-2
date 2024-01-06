53df5ce28c0db684043a88660b8041af587fcbbc
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-06 13:45:57 +0200

    Add files via upload

---
+++ Ex11/Program.cs
@@ -0,0 +1,24 @@
+﻿//Se da o secventa de n numere. Se cere sa se caculeze suma inverselor acestor numere. 
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Introduceti numarul de elemente din secventa:");
+        int n = int.Parse(Console.ReadLine());
+
+        double sumaInverselor = 0.0;
+
+        Console.WriteLine("Introduceti secventa de numere:");
+
+        for (int i = 0; i < n; i++)
+        {
+            double numar = double.Parse(Console.ReadLine());
+            sumaInverselor += 1.0 / numar;
+        }
+
+        Console.WriteLine($"Suma inverselor este: {sumaInverselor}");
+    }
+}
