563a56f9a20b7da3d761152b57bab68317263c7b
clepcevanesa <155635810+clepcevanesa@users.noreply.github.com>
2024-01-06 13:32:43 +0200

    Add files via upload

---
+++ Ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿//Cate elemente dintr-o secventa de n numere sunt egale cu pozitia pe care apar in secventa. Se considera ca primul element din secventa este pe pozitia 0. 
+
+using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        int n = 10; 
+        int[] secventa = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            secventa[i] = i; 
+        }
+
+        // Afișați elementele care sunt egale cu poziția lor în secvență
+        Console.WriteLine("Elementele care sunt egale cu pozitia lor in secventa sunt:");
+        for (int i = 0; i < n; i++)
+        {
+            if (secventa[i] == i)
+            {
+                Console.WriteLine($"Elementul {secventa[i]} se afla pe pozitia {i} în secventa.");
+            }
+        }
+    }
+}
